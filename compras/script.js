fd4e5844a87050ecf75feaa3dce90a65c0fd64ac
nachito0~ <ignacio.barriga.munoz@alumnos.sip.cl>
2023-06-05 15:16:16 -0400

    15

--- compras/script.js
+++ compras/script.js
@@ -3,29 +3,3 @@ window.addEventListener('scroll', function() {
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     derElement.style.transform = 'translateY(' + scrollTop + 'px)';
   });
-
-  
-  function addTicket() {
-    var seatSelect = document.getElementById("seat");
-    var selectedSeat = seatSelect.value;
-    
-    if (selectedSeat !== "") {
-      var table = document.getElementById("ticket-table");
-      var row = table.insertRow();
-      
-      var seatCell = row.insertCell(0);
-      seatCell.innerHTML = selectedSeat;
-      
-      var actionsCell = row.insertCell(1);
-      actionsCell.innerHTML = "<button class='button' onclick='deleteTicket(this)'>Eliminar</button>";
-      
-      seatSelect.value = "";
-    }
-  }
-  
-  function deleteTicket(button) {
-    var row = button.parentNode.parentNode;
-    var table = row.parentNode;
-    
-    table.deleteRow(row.rowIndex);
-  }
