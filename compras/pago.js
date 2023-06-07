10feee64e82fa194925738190bc9ec25714d90d6
nachito0~ <ignacio.barriga.munoz@alumnos.sip.cl>
2023-06-07 13:25:17 -0400

    13

--- compras/pago.js
+++ compras/pago.js
@@ -1,24 +1,12 @@
-const form = document.getElementById('paymentForm');
+document.addEventListener("DOMContentLoaded", function() {
+    var continuarBtn = document.getElementById("continuar-btn-der");
+    var webpayRadio = document.getElementById("webpay");
 
-    // Agregar un controlador de eventos para el envío del formulario
-    form.addEventListener('submit', function(event) {
-      event.preventDefault(); // Prevenir el envío del formulario
-
-      // Obtener el valor del método de pago seleccionado
-      const metodoPago = document.querySelector('input[name="metodo_pago"]:checked');
-
-      if (metodoPago) {
-        const metodoPagoSeleccionado = metodoPago.value;
-        // Aquí puedes agregar lógica adicional en función del método de pago seleccionado
-        alert("Has seleccionado el método de pago: " + metodoPagoSeleccionado);
-        // Aquí puedes redirigir al usuario a la página de pago correspondiente
-      } else {
-        alert("Por favor, selecciona un método de pago");
-      }
-
-    window.addEventListener('scroll', function() {
-      var derElement = document.getElementById('der-element');
-      var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      derElement.style.transform = 'translateY(' + scrollTop + 'px)'; 
-    })
-  })
+    continuarBtn.addEventListener("click", function(event) {
+        if (!webpayRadio.checked) {
+            alert("Por favor, selecciona un metodo de pago");
+        } else {
+            window.location.href = "https://www.webpay.cl";
+        }
+    });
+});
